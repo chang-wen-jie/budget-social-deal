dd735b4660853a09fa6a3d3ab34902ab93bbd609
Chang Wen Jie <changwenjie1999@gmail.com>
2022-12-26 22:44:17 +0100

    mat-label edit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,21 +6,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  title = 'vouchers';
   products: any;
   filteredProducts: any;
+  tabs = [
+    { label: 'All'},
+    { label: 'Smartphones'},
+    { label: 'Laptops'},
+    { label: 'Fragrances'}
+  ];
+  test = [{
+    label: 'all',
+  }];
   
   async getProducts() {
     let url = 'https://dummyjson.com/products'
     let obj = await (await fetch(url)).json();
     this.products = obj['products'];
+    Object.keys(obj['products']).forEach((k) => {
+      let category = obj['products'][k]['category']
+      if (!this.test.includes({label: category})) {
+        console.log('test:', this.test[0]);
+        this.test.push({label: category});
+      }
+    })
+    this.filteredProducts = this.products;
   }
 
-  test(selectedTab: any) {
-    let filter = selectedTab['tab']['textLabel'];
-    this.filteredProducts = this.products.filter((product: { ['category']: string }) => product['category'] === filter);
+  selectTab(selectedTab: any) {
+    let tab = selectedTab['tab']['textLabel'].toLowerCase();
+    if (selectedTab['index'] === 0) {
+      this.filteredProducts = this.products;
+    } else {
+      this.filteredProducts = this.products.filter((product: { ['category']: string }) => product['category'] === tab);
+    }
   }
-
 
   public ngOnInit(): void {
     this.getProducts();
