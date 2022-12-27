7df6bc58f7d7c48f66fb98b406e3a4233f6d76b8
Chang Wen Jie <changwenjie1999@gmail.com>
2022-12-27 22:34:10 +0100

    styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,41 +3,41 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
   products: any;
   filteredProducts: any;
-  tabs = [
-    { label: 'All'},
-    { label: 'Smartphones'},
-    { label: 'Laptops'},
-    { label: 'Fragrances'}
-  ];
-  test = [{
-    label: 'all',
-  }];
-  
+  productTabs = [{ label: 'All' }];
+
   async getProducts() {
-    let url = 'https://dummyjson.com/products'
+    let url = 'https://dummyjson.com/products';
     let obj = await (await fetch(url)).json();
     this.products = obj['products'];
+
     Object.keys(obj['products']).forEach((k) => {
-      let category = obj['products'][k]['category']
-      if (!this.test.includes({label: category})) {
-        console.log('test:', this.test[0]);
-        this.test.push({label: category});
-      }
-    })
+      let category = obj['products'][k]['category'];
+      this.productTabs.push({
+        label: category.charAt(0).toUpperCase() + category.slice(1),
+      });
+    });
+
+    this.productTabs = this.productTabs.filter(
+      (obj, index) =>
+        this.productTabs.findIndex((o) => o.label === obj.label) === index
+    );
     this.filteredProducts = this.products;
   }
 
   selectTab(selectedTab: any) {
     let tab = selectedTab['tab']['textLabel'].toLowerCase();
+
     if (selectedTab['index'] === 0) {
       this.filteredProducts = this.products;
     } else {
-      this.filteredProducts = this.products.filter((product: { ['category']: string }) => product['category'] === tab);
+      this.filteredProducts = this.products.filter(
+        (product: { ['category']: string }) => product['category'] === tab
+      );
     }
   }
 
